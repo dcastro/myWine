c3a31514722023c19dd6d9f9d6ce5326f293313a
André Dias <andre.g.dias@gmail.com>
2012-05-13 20:27:33 +0100

    adicionada classe de testes
    Ainda não faz nenhum teste

---
+++ Tests/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by André Dias on 13/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
