1d57eafb3251b542a833cf3adec2e388d5a6e4d0
Fernando Graças <fgracas89@gmail.com>
2012-05-16 16:22:30 +0100

    Delete das tastings feito na BD

--- myWine/NSMutableArray+ProvasMutableArray.m
+++ myWine/NSMutableArray+ProvasMutableArray.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSMutableArray+ProvasMutableArray.h"
+#import "Query.h"
 
 @implementation NSMutableArray (ProvasMutableArray)
 
@@ -15,10 +16,72 @@
 }
 
 -(BOOL) removeProvaAtIndex:(NSUInteger) index {
-    #warning TODO: completar este metodo
+    Prova * p = [self objectAtIndex:index];
+    Query *query = [[Query alloc] init];
     
-    [self removeObjectAtIndex:index];
-    return TRUE;
+    BOOL return_value = TRUE;
+    sqlite3_stmt *stmt;
+    char * errMsg;
+    
+    
+    NSString *querySQL = [NSString stringWithFormat:@"SELECT state FROM Tasting WHERE tasting_id = %d", p.tasting_id];
+    
+    sqlite3** contactDB = [query prepareForExecution];
+    
+    if (sqlite3_prepare_v2(*contactDB, [querySQL UTF8String], -1, &stmt, NULL) == SQLITE_OK){
+        
+        int state;
+        
+        if(stmt != NULL){
+            if(sqlite3_step(stmt) == SQLITE_ROW){
+                state = sqlite3_column_int(stmt, 0);
+                sqlite3_finalize(stmt);
+                
+                switch (state) {
+                    case 0:
+                        querySQL = [NSString stringWithFormat:@"UPDATE Tasting SET state = 3 WHERE wine_id = %d", p.tasting_id];
+                        if(sqlite3_exec(*contactDB, [querySQL UTF8String], NULL, NULL, &errMsg) != SQLITE_OK){
+                            DebugLog(@"Could not mark for deletion: %s", errMsg);
+                            sqlite3_free(errMsg);
+                            return_value = FALSE;
+                        }
+                        break;
+                        
+                    case 1:
+                        querySQL = [NSString stringWithFormat:@"DELETE FROM Tasting WHERE tasting_id = %d", p.tasting_id];
+                        if(sqlite3_exec(*contactDB, [querySQL UTF8String], NULL, NULL, &errMsg) != SQLITE_OK){
+                            DebugLog(@"Could not delete: %s", errMsg);
+                            sqlite3_free(errMsg);
+                            return_value = FALSE;
+                        }
+                        break;
+                        
+                    case 2:
+                        querySQL = [NSString stringWithFormat:@"UPDATE Tasting SET state = 3 WHERE tasting_id = %d", p.tasting_id];
+                        if(sqlite3_exec(*contactDB, [querySQL UTF8String], NULL, NULL, &errMsg) != SQLITE_OK){
+                            DebugLog(@"Could not mark for deletion: %s", errMsg);
+                            sqlite3_free(errMsg);
+                            return_value = FALSE;
+                        }
+                        break;
+                        
+                    default:
+                        break;
+                }
+            }
+            sqlite3_close(*contactDB);
+        }
+        
+    }else{
+        return_value = FALSE;
+        sqlite3_close(*contactDB);
+    }
+    
+    if(return_value)
+        [self removeObjectAtIndex:index];
+    
+    return return_value;
+
 }
 
 @end
