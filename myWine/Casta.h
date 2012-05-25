2546e5f8f3a1754de3ae1fabe0ec00c77ab2223a
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-05-25 20:15:26 +0100

    Revert "Eliminada a classe Casta"
    This reverts commit f0b161b9eff18e10787f3719f9a1e54dfc15a1ef.

---
+++ myWine/Casta.h
@@ -0,0 +1,16 @@
+//
+//  Casta.h
+//  myWine
+//
+//  Created by Fernando Gracas on 5/20/12.
+//  Copyright (c) 2012 FEUP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Casta : NSObject
+
+@property (nonatomic, assign) int grape_id;
+@property (nonatomic, copy) NSString *grape_name;
+
+@end
