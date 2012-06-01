6c5d29cbc90ed8fd8d4d3dca174f379309810b51
António Velasquez <sergio.velasquez@gmail.com>
2012-06-01 16:47:15 +0100

    camera changes

--- myWine/FotoViewController.h
+++ myWine/FotoViewController.h
@@ -7,10 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface FotoViewController : UIViewController <UIImagePickerControllerDelegate>
+@interface FotoViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
+    
+    UIImageView *imageView;
+    BOOL newMedia;
+}
 
 - (IBAction)cancel:(id)sender;
 - (IBAction)done:(id)sender;
 
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) UIPopoverController *myPop;
 @end
--- myWine/FotoViewController.m
+++ myWine/FotoViewController.m
@@ -13,37 +13,20 @@
 @end
 
 @implementation FotoViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+@synthesize imageView;
+@synthesize myPop;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
-    
-    // Set source to the camera (needs real iPad !!)
-    //imagePicker.sourceType =  UIImagePickerControllerSourceTypeCamera;
-    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    
-    // Delegate is self
-    //imagePicker.delegate = self;
-    
-    // Show image picker
-    //[self presentModalViewController:imagePicker animated:YES];
-    
     // Do any additional setup after loading the view.
+
 }
 
 - (void)viewDidUnload
 {
+    self.imageView = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -53,14 +36,102 @@
     return YES;
 }
 
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissModalViewControllerAnimated:YES];
+}
+
 - (IBAction)cancel:(id)sender
 {
 	[self dismissModalViewControllerAnimated:YES];
 }
+
 - (IBAction)done:(id)sender
 {
-	// TODO
+    if ([UIImagePickerController isSourceTypeAvailable:
+         UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *imagePicker =
+        [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+        imagePicker.sourceType = 
+        UIImagePickerControllerSourceTypeCamera;
+        imagePicker.mediaTypes = [NSArray arrayWithObjects:
+                                  (NSString *) kUTTypeImage,
+                                  nil];
+        imagePicker.allowsEditing = NO;
+        [self presentModalViewController:imagePicker 
+                                animated:YES];
+        newMedia = YES;
+    }
+    else if ([UIImagePickerController isSourceTypeAvailable:
+         UIImagePickerControllerSourceTypeSavedPhotosAlbum])
+    {
+        UIImagePickerController *imagePicker =
+        [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+        imagePicker.sourceType = 
+        UIImagePickerControllerSourceTypePhotoLibrary;
+        imagePicker.mediaTypes = [NSArray arrayWithObjects:
+                                  (NSString *) kUTTypeImage,
+                                  nil];
+        imagePicker.allowsEditing = NO;
+       // [self presentModalViewController:imagePicker animated:YES];
+       [self.myPop presentPopoverFromBarButtonItem:sender
+       permittedArrowDirections:UIPopoverArrowDirectionUp
+       animated:YES];
+        
+        newMedia = NO;
+        NSLog(@"I have library");
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle: @"Camera not supported"
+                              message: @"No camera"\
+                              delegate: nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+        [alert show];
+
+    }
 }
 
+-(void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSString *mediaType = [info
+                           objectForKey:UIImagePickerControllerMediaType];
+    [self dismissModalViewControllerAnimated:YES];
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
+        UIImage *image = [info 
+                          objectForKey:UIImagePickerControllerOriginalImage];
+        
+        imageView.image = image;
+        if (newMedia)
+            UIImageWriteToSavedPhotosAlbum(image, 
+                                           self,
+                                           @selector(image:finishedSavingWithError:contextInfo:),
+                                           nil);
+    }
+    else if ([mediaType isEqualToString:(NSString *)kUTTypeMovie])
+    {
+		// Code here to support video if enabled
+	}
+}
+
+-(void)image:(UIImage *)image
+finishedSavingWithError:(NSError *)error 
+ contextInfo:(void *)contextInfo
+{
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle: @"Save failed"
+                              message: @"Failed to save image"\
+                              delegate: nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
 
 @end
