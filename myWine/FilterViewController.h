9c83a918b976e05fa72f64c550191b98fa765d0f
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-06-06 15:39:13 +0100

    Filter VC criado com custom rows

---
+++ myWine/FilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  FilterViewController.h
+//  myWine
+//
+//  Created by Diogo on 6/6/12.
+//  Copyright (c) 2012 FEUP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewController : UITableViewController
+
+@end
