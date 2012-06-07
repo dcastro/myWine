53328ca2a1fa218860e7aa28e28bf48625fd9ec6
Nuno Sousa <nmlpsousa@gmail.com>
2012-06-07 15:39:41 +0100

    OrderViewController adicionado

---
+++ myWine/OrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrderViewController.h
+//  myWine
+//
+//  Created by Nuno Sousa on 07/06/12.
+//  Copyright (c) 2012 FEUP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderViewController : UITableViewController
+
+@end
